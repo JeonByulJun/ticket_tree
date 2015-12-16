37e5129f7e2bb5e61d9fc6231ece598b43a760c4
hyunjunian <hyunjunian@gmail.com>
2015-12-16 08:07:04 +0000

    main design

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20151119183349) do
 
   create_table "users", force: :cascade do |t|
     t.string   "img"
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
